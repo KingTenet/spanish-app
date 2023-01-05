a7aa131604a37560aff58a9d02ff931d13af56fa
KingTenet <30955169+KingTenet@users.noreply.github.com>
2023-01-05 15:10:40 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello cheese fdsa fdasfd whoops CodeSandbox</h1>
+      <h2>Stafdsafdsrtfghgf fhgfediting to see some magic happen!</h2>
+    </div>
+  );
+}
