f810a3d8c81e166094dd039c9d1370c26d62627c
KingTenet <30955169+KingTenet@users.noreply.github.com>
2023-01-05 15:20:02 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,42 @@ import App from "./App";
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
+const api_key = "AIzaSyBUWtWQ1o5xMJd1_-y4PAteRHP3Yq_5mhY";
+
+function getTabUrl(spreadsheet_id, tab_name) {
+  return (
+    "https://sheets.googleapis.com/v4/spreadsheets/" +
+    spreadsheet_id +
+    "/values/" +
+    encodeURIComponent(tab_name) +
+    "?alt=json&key=" +
+    api_key
+  );
+}
+
+function getAllTabsUrl(spreadsheet_id) {
+  return (
+    "https://sheets.googleapis.com/v4/spreadsheets/" +
+    spreadsheet_id +
+    "?alt=json&key=" +
+    api_key
+  );
+}
+
+async function getJson(url) {
+  console.log(url);
+  return await fetch(url).then((response) => response.json());
+}
+
+async function main() {
+  var spreadsheet_id = "1ZXAX4K2rjLQHl4bbidDAgScilH3khcRcLCAl0fATbwk";
+
+  const allTabsJson = await getJson(getAllTabsUrl(spreadsheet_id));
+  console.log(allTabsJson);
+}
+
+main().catch((err) => console.log(err));
+
 root.render(
   <StrictMode>
     <App />
